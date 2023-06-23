a9f0aae829156ecd8e24a84f4e52f7f400ef55c4
sundhar22 <s.mohanasundharam2210@gmail.com>
2023-06-23 18:14:44 +0530

    first commit

---
+++ lib/presentation/themes/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+  static const Color purplyBlue = Color(0xFF6f65dc);
+  static const Color bluishCyan = Color(0xFF7ed1df);
+  static const Color santaGrey = Color(0xFFa0a3b9);
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+}
