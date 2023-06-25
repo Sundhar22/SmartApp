f18779372fa21187d4ff1fc03d8bec3b886fd2c8
Sanjai P <sanjaiprabhakaran.official@gmail.com>
2023-06-25 19:30:11 +0530

    fitnesslevel

--- lib/presentation/themes/app_colors.dart
+++ lib/presentation/themes/app_colors.dart
@@ -5,6 +5,7 @@ class AppColor {
   static const Color purplyBlue = Color(0xFF6f65dc);
   static const Color bluishCyan = Color(0xFF7ed1df);
   static const Color santaGrey = Color(0xFFa0a3b9);
+  static const Color lightPurplrBlue = Color.fromARGB(255, 229, 223, 254);
   static const Color black = Colors.black;
   static const Color white = Colors.white;
 }
